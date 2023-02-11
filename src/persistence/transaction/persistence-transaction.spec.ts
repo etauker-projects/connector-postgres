73d3d17ce4c68756f5667d892dca95707472127d
Tautvydas Kersulis <etauker@gmail.com>
2023-02-11 23:04:13 +0000

    Add unit tests

--- src/persistence/transaction/persistence-transaction.spec.ts
+++ src/persistence/transaction/persistence-transaction.spec.ts
@@ -7,6 +7,7 @@ import { IPoolClient } from '../../postgres/model/postgres-pool-client.interface
 describe('PersistenceTransaction', () => {
 
     let client: IPoolClient;
+    let transaction: PersistenceTransaction;
     let mock: {
         query: sinon.SinonStub;
         release: sinon.SinonStub;
@@ -34,11 +35,11 @@ describe('PersistenceTransaction', () => {
                 }),
         };
         client = mock as any as IPoolClient;
+        transaction = new PersistenceTransaction(Promise.resolve(client));
     })
 
     describe('constructor', () => {
         it('should begin transaction', () => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             return transaction.ready().then(() => {
                 assert(mock.query.calledOnce, 'query should be called once but was called ' + mock.query.callCount + ' time(s)');
                 assert(mock.query.calledWith('BEGIN'), `should be called with 'BEGIN'`);
@@ -48,7 +49,6 @@ describe('PersistenceTransaction', () => {
 
     describe('continue', () => {
         it('should forward correct parameters to database client', () => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             const sql = 'SELECT name FROM test WHERE id = ?';
             const params = [ 'abcd' ];
 
@@ -62,7 +62,6 @@ describe('PersistenceTransaction', () => {
             ;
         })
         it('should throw exception if transaction is already complete', (done) => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             const sql = 'SELECT name FROM test WHERE id = ?';
             const params = [ 'abcd' ];
 
@@ -83,7 +82,6 @@ describe('PersistenceTransaction', () => {
 
     describe('end', () => {
         it('(commit = true) => should end transaction with a commit', () => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             return transaction.end(true).then(() => {
                 assert(mock.query.calledTwice, 'query should be called twice but was called ' + mock.query.callCount + ' time(s)');
                 const secondCallArg = mock.query.getCall(1).args[0];
@@ -91,13 +89,11 @@ describe('PersistenceTransaction', () => {
             })
         })
         it('(commit = true) => should release a connection after a commit', () => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             return transaction.end(true).then(() => {
                 assert(mock.release.calledOnce, 'release should be called once but was called ' + mock.release.callCount + ' time(s)');
             })
         })
         it('(commit = true) => should throw exception if transaction is already complete', (done) => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             transaction.ready()
                 .then(() => transaction.end(true))
                 .then(() => transaction.end(true))
@@ -112,7 +108,6 @@ describe('PersistenceTransaction', () => {
             ;
         })
         it('(commit = false) => should end transaction with a rollback', () => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             return transaction.end(false).then(() => {
                 assert(mock.query.calledTwice, 'query should be called twice but was called ' + mock.query.callCount + ' time(s)');
                 const secondCallArg = mock.query.getCall(1).args[0];
@@ -120,13 +115,11 @@ describe('PersistenceTransaction', () => {
             })
         })
         it('(commit = false) => should release a connection after a rollback', () => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             return transaction.end(false).then(() => {
                 assert(mock.release.calledOnce, 'release should be called once but was called ' + mock.release.callCount + ' time(s)');
             })
         })
         it('(commit = false) => should throw exception if transaction is already complete', (done) => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             transaction.ready()
                 .then(() => transaction.end(false))
                 .then(() => transaction.end(false))
@@ -144,7 +137,6 @@ describe('PersistenceTransaction', () => {
 
     describe('commit', () => {
         it('should end transaction with a commit', () => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             return transaction.commit().then(() => {
                 assert(mock.query.calledTwice, 'query should be called twice but was called ' + mock.query.callCount + ' time(s)');
                 const secondCallArg = mock.query.getCall(1).args[0];
@@ -152,13 +144,11 @@ describe('PersistenceTransaction', () => {
             })
         })
         it('should release a connection after a commit', () => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             return transaction.commit().then(() => {
                 assert(mock.release.calledOnce, 'release should be called once but was called ' + mock.release.callCount + ' time(s)');
             })
         })
         it('should throw exception if transaction is already complete', (done) => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             transaction.ready()
                 .then(() => transaction.commit())
                 .then(() => transaction.commit())
@@ -176,7 +166,6 @@ describe('PersistenceTransaction', () => {
 
     describe('rollback', () => {
         it('should end transaction with a rollback', () => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             return transaction.rollback().then(() => {
                 assert(mock.query.calledTwice, 'query should be called twice but was called ' + mock.query.callCount + ' time(s)');
                 const secondCallArg = mock.query.getCall(1).args[0];
@@ -184,13 +173,11 @@ describe('PersistenceTransaction', () => {
             })
         })
         it('should release a connection after a rollback', () => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             return transaction.rollback().then(() => {
                 assert(mock.release.calledOnce, 'release should be called once but was called ' + mock.release.callCount + ' time(s)');
             })
         })
         it('should throw exception if transaction is already complete', (done) => {
-            const transaction = new PersistenceTransaction(Promise.resolve(client));
             transaction.ready()
                 .then(() => transaction.rollback())
                 .then(() => transaction.rollback())
@@ -205,4 +192,62 @@ describe('PersistenceTransaction', () => {
             ;
         })
     })
+
+    describe('isOpen', () => {
+        it('should return true before query (connection not closed)', async () => {
+            assert.strictEqual(await transaction.isOpen(), true);
+        })
+        it('should return true after query (connection not closed)', async () => {
+            await transaction.continue('SELECT true');
+            assert.strictEqual(await transaction.isOpen(), true);
+        })
+        it('should return false after commit', async () => {
+            await transaction.commit();
+            assert.strictEqual(await transaction.isOpen(), false);
+        })
+        it('should return false after rollback', async () => {
+            await transaction.rollback();
+            assert.strictEqual(await transaction.isOpen(), false);
+        })
+        it('should return false after end (commit = true)', async () => {
+            await transaction.end(true);
+            assert.strictEqual(await transaction.isOpen(), false);
+        })
+        it('should return false after end (commit = false)', async () => {
+            await transaction.end(false);
+            assert.strictEqual(await transaction.isOpen(), false);
+        })
+    })
+
+    describe('closeIfOpen', () => {
+        it('should close connection if one was open (commit = true)', async () => {
+            assert.strictEqual(await transaction.isOpen(), true);
+            await transaction.closeIfOpen(true);
+            assert.strictEqual(await transaction.isOpen(), false);
+            assert(mock.release.calledOnce, 'release should be called once but was called ' + mock.release.callCount + ' time(s)');
+        })
+        it('should close connection if one was open (commit = false)', async () => {
+            assert.strictEqual(await transaction.isOpen(), true);
+            await transaction.closeIfOpen(false);
+            assert.strictEqual(await transaction.isOpen(), false);
+            assert(mock.release.calledOnce, 'release should be called once but was called ' + mock.release.callCount + ' time(s)');
+        })
+        it('should not throw error if commit was already called', async () => {
+            assert.strictEqual(await transaction.isOpen(), true);
+            await transaction.commit();
+            assert.strictEqual(await transaction.isOpen(), false);
+            await transaction.closeIfOpen(true);
+            assert.strictEqual(await transaction.isOpen(), false);
+            assert(mock.release.calledOnce, 'release should be called once but was called ' + mock.release.callCount + ' time(s)');
+        })
+        it('should not throw error if rollback was already called', async () => {
+            assert.strictEqual(await transaction.isOpen(), true);
+            await transaction.rollback();
+            assert.strictEqual(await transaction.isOpen(), false);
+            await transaction.closeIfOpen(true);
+            assert.strictEqual(await transaction.isOpen(), false);
+            assert(mock.release.calledOnce, 'release should be called once but was called ' + mock.release.callCount + ' time(s)');
+        })
+    })
+
 })
